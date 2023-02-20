bc51c5a71f1681200dd271c3a97d6068d8cc3218
Ivan Rodin <ivs.rodin@gmail.com>
2023-02-20 23:10:34 +0100

    Add get_slice init

--- ivs_pyomo_features.py
+++ ivs_pyomo_features.py
@@ -1,6 +1,15 @@
+from pyomo.environ import ConcreteModel
+
 __version__ = 'dev'
 
 #TODO: Base class pyomo model
 #TODO: Impvars
-class PyomoSet():
-    s = set()
+
+class PyomoSet(set):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+    def get_slice(self, mask):
+        print(mask)
+        for element in self:
+            print(element)
